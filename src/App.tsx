df31992f7b5892dcffebcbab3aebbd7172479c12
kaifx-ai <khankaif8794@gmail.com>
2025-03-15 00:25:51 +0530

    Changes: Projects Initiated & Setup Completed with ShadCn UI

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+const App = () => {
+  return (
+    <div className='text-blue-500'>
+
+    <Button>Click Me</Button>
+  </div>
+  )
+};
+
+export default App;
